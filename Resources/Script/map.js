e6db1708118889514e64ab2ffe9500faa87287b4
Pranav21-pav <pranav.swatantran@gmail.com>
2024-04-13 19:43:46 -0500

    Tried to interact with map

--- Resources/Script/map.js
+++ Resources/Script/map.js
@@ -1,18 +1,10 @@
-$(document).ready(function() {
-    $('a').click(function(event) {
-      event.preventDefault();
-      $(this).hide("slow");
-    });
-  });
-
-// Define the moveMap function to move the Google Map Embedded to the area specified by the zip code
-function moveMap() {
-    // Retrieve the zip code entered by the user from the input field
-    var zipCode = document.getElementById("zipCode").value;
-    
-    // Construct the URL for the Google Map Embedded with the new location based on the zip code
-    var mapURL = "https://maps.google.com/maps?q=" + zipCode + "&output=embed";
-    
-    // Update the iframe src attribute to load the new map with the specified location
-    document.getElementById("mapFrame").src = mapURL;
-}
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
+        ({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "weekly"});
+      
+function Map() {
+  var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+  };
+  var map = new google.maps.Map(document.getElementById("Map"),mapProp);
+  }
